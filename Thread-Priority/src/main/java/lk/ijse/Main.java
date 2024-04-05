da4ee6e867a17e21fadbe5d4830d96d6ac589d33
USER <sithiraroneth@gmail.com>
2024-04-05 11:33:48 +0530

    add join-isAlive

---
+++ Thread-Priority/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,12 @@
+/* Created By Sithira Roneth
+ * Date :4/5/24
+ * Time :11:33
+ * Project Name :Thread
+ * */
+package lk.ijse;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
