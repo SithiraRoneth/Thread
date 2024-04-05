1fbd5d0b9a0b943c80dda9e392bc22dc809a175f
USER <sithiraroneth@gmail.com>
2024-04-05 10:52:29 +0530

    add Lambda-Expression

---
+++ Lamda-Exprestion/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,49 @@
+/* Created By Sithira Roneth
+ * Date :4/5/24
+ * Time :10:42
+ * Project Name :Thread
+ * */
+package lk.ijse;
+
+public class Main {
+    public static void main(String[] args) {
+        Runnable obj1 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("One");
+                    try{
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        System.out.println(e);
+                    }
+                }
+            }
+        };
+
+        Runnable obj2 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Two");
+                    try{
+                        Thread.sleep(500);
+                    }catch (InterruptedException e){
+                        System.out.println(e);
+                    }
+                }
+            }
+        };
+
+        Thread one = new Thread(obj1);
+        Thread two = new Thread(obj2);
+
+        one.start();
+        try{
+            Thread.sleep(10);
+        }catch (InterruptedException e){
+            throw new RuntimeException();
+        }
+        two.start();
+    }
+}
